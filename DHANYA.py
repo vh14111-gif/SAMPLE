a59840bd3233d7385f3ef24982bae3018e7ab03f
Digital Library <vh14111@velhightech.com>
2026-04-29 12:44:36 +0530

    First commit

---
+++ DHANYA.py
@@ -0,0 +1,14 @@
+# simple_program.py
+
+def greet(name):
+    print("Hello,", name)
+
+def add(a, b):
+    return a + b
+
+# main program
+if __name__ == "__main__":
+    greet("Dhanya")
+    
+    result = add(5, 3)
+    print("Sum is:", result)
